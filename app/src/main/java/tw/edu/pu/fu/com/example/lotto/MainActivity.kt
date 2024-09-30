e5842a0d18dfd571ae54b044ffec3edbefd58281
fu24680 <qwe11222017@gmail.com>
2024-09-30 15:47:01 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/fu/com/example/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/fu/com/example/lotto/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.fu.com.example.lotto
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,5 +23,9 @@ class MainActivity : AppCompatActivity() {
         txv.text = "0"
 
     }
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
 
+    }
 }
